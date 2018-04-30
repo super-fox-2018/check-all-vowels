dd957e6c82d9926c07702eaabfffa3cf84f39a79
ariefardi <forzaariefpyon@gmail.com>
2018-04-30 22:39:53 +0700

    check vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
+let cekVowels = function(str){
+    let vowels = 'aiueo';
+    let count = 0;
+    for(let i=0;i<str.length;i++){
+        for(let j=0;j<vowels.length;j++){
+            if(str[i].toLowerCase()===vowels[j]){
+                count++;
+            }
+        }
+    }
+    if(str.length === count){
+        return true;
+    }else{
+        return false;
+    }
+}
 
+console.log(cekVowels('aiEEEueo')) //true
+console.log(cekVowels('aiu')) //true
+console.log(cekVowels('budi')) //false
+console.log(cekVowels('asep')) //false
