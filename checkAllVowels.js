7bfe8db62bae9dead6ead6043f5167c3db792b30
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-05-01 00:51:08 +0700

    Check All Vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,27 @@
-
+function checkVowels(str) {
+    var countVowels=0
+    for (var i=0; i<str.length; i++){
+        if (str[i]==="a" || str[i]==="A"){
+            countVowels+=1
+        }
+        else if (str[i]==="i" || str[i]==="I"){
+            countVowels+=1
+        }
+        else if (str[i]==="u" || str[i]==="U"){
+            countVowels+=1
+        }
+        else if (str[i]==="e" || str[i]==="E"){
+            countVowels+=1
+        }
+        else if (str[i]==="o" || str[i]==="O"){
+            countVowels+=1
+        }
+    }
+    if (countVowels === str.length) return true
+        return false
+  }
+  
+  console.log(checkVowels('aiueo')); // true
+  console.log(checkVowels('AiUEo')); // true
+  console.log(checkVowels('aIUko')); // false
+  
