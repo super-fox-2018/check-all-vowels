dc77e20e1fa5b66ec1ba32087a00afa2eb5c97d7
alprak <you@example.com>
2018-05-01 15:33:35 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
+function checkAllVowels(str) {
+	var vokals = ["a","i","u","e","o"]
+	for(var i=0;i<str.length;i++) {
+		var status=false;
+		for(var j=0;j<vokals.length;j++) {
+			if(str[i].toLowerCase() == vokals[j]) {
+				status = true
+				return status
+			}
+		}
+	}
+	return status
+}
 
+console.log(checkAllVowels("HAlls"))
+console.log(checkAllVowels("Hslls"))
