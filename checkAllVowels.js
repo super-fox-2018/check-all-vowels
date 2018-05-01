d9a56d98c3ce7ae20c05b03dfa788255451aa849
Dwiky Erlangga <dwikyerl@gmail.com>
2018-05-01 08:16:29 +0700

    Finish Check All Vowels Challenge

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+function checkAllVowels(str) {
+  const vowels = 'aeiou';
+  for (let i = 0; i < str.length; i += 1) {
+    let isVowel = false;
+    for (let j = 0; j < vowels.length; j += 1) {
+      if (str[i] === vowels[j]) {
+        isVowel = true;
+        break;
+      }
+    }
 
+    if (!isVowel) return false;
+  }
+  
+  return true;
+}
+
+console.log(checkAllVowels('uoiauai')); // true
+console.log(checkAllVowels('hacktiv8')); // false
