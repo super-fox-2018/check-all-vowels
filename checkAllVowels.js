ade79930f775dcfa1b571a657063189a8f657d24
alprak <you@example.com>
2018-05-01 18:48:15 +0700

    revisi

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,16 +1,21 @@
 function checkAllVowels(str) {
 	var vokals = ["a","i","u","e","o"]
+	var count=0;
 	for(var i=0;i<str.length;i++) {
-		var status=false;
 		for(var j=0;j<vokals.length;j++) {
 			if(str[i].toLowerCase() == vokals[j]) {
-				status = true
-				return status
-			}
+				count++
+			}	
 		}
 	}
-	return status
+	if(count == str.length) {
+		return true
+	}else {
+		return false
+	}
 }
 
-console.log(checkAllVowels("HAlls"))
+console.log(checkAllVowels("UEOI"))
 console.log(checkAllVowels("Hslls"))
+console.log(checkAllVowels("AEIO"))
+console.log(checkAllVowels("AEIS"))
