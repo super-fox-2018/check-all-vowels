e32c5ebcea4e1a82637071130b4927e30c402497
mario <panggihmario@gmail.com>
2018-05-01 16:01:13 +0700

    cekVocal

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,25 @@
 
+function checkVowels(str){
+    var vocal='aiueo'
+    var kecil=str.toLowerCase()
+    var count =0
+    for(let i=0;i<kecil.length;i++){   
+        for(let j=0;j<vocal.length;j++){
+            if(kecil[i] === vocal[j]){
+                count++
+               
+            }
+           
+        }     
+    }
+    if(count === kecil.length){
+        return true
+    }
+        return false
+
+
+}
+console.log(checkVowels('aio'))
+console.log(checkVowels('abc'))
+console.log(checkVowels('AoaIeOo'))
+console.log(checkVowels('AIuTP'))
