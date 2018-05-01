938c59f8bca81698d05334342966e1e8c8c5f72f
justinusamwijaya <justin.am.wijaya@gmail.com>
2018-05-01 12:05:16 +0700

    jawaban viewallvocals

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,14 @@
 
+var vowelChecker = (x) =>{
+    var comparator = 'bcdfghjklmnpqrstvwxyz'.split('')
+    for(i=0;i<x.length;i++){
+        for(j=0;j<comparator.length;j++){
+            if(x.charAt(i)==comparator[j]) return false
+        }
+    }
+    return true
+}
+
+
+console.log(vowelChecker("aaaaa"))
+console.log(vowelChecker('aaaaaab'))
