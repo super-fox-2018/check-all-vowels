d80ca12a0fabcc5c327dd4eee73d6f39d556523c
Rhesa Utomo <venderblitz@gmail.com>
2018-05-01 18:54:49 +0700

    finished checkAllVowels.js

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,18 @@
+function checkVowels(str) {
+  var string = str.toLowerCase();
+  var count = 0;
 
+  for (var i = 0; i < str.length; i++) {
+    if (string[i] === 'a' || string[i] === 'e' || string[i] === 'i' ||
+        string[i] === 'o' || string[i] === 'u') {
+      count++;
+    }
+  }
+
+  if (count === str.length) return true;
+  return false;
+}
+console.log(checkVowels('ABCDE'));
+console.log(checkVowels('aEIuOOaai'));
+console.log(checkVowels('aiuoe'));
+console.log(checkVowels('hacktiv8'));
