4b64f3faf1c562d5e9613dd63791f925d41b3c53
Michael Cangcianno <michaelcangcianno@Michaels-MacBook-Pro.local>
2018-05-01 11:06:49 +0700

    finished check all vowels problem

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkVowels(str) {
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let isVowel = false;
+  for (var i = 0; i < str.length; i++) {
+    isVowel = false;
+    for (var j = 0; j < vowels.length; j++) {
+      if (str[i].toLowerCase() === vowels[j].toLowerCase()) {
+        isVowel = true;
+        break;
+      }
+    }
+    if (isVowel === false) {
+      return false;
+    }
+  }
+  return true;
+}
 
+console.log(checkVowels('aBEoi'));
+console.log(checkVowels('aoEUi'));
