5ba7cdba85ff119a7337b60dc11d8d0a36def722
shantidyah <shanti.dyah@gmail.com>
2018-05-01 22:16:45 +0700

    check All Vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,17 @@
-
+function checkAllVowels(string) {
+  var vocal = 'aiueo';
+  for (var i = 0; i < string.length; i++) {
+    var status = false;
+    for (var j = 0; j < vocal.length; j++) {
+      if (string[i].toLowerCase()==vocal[j]) {
+        status = true;
+      }
+    }
+    if(status==false){
+      return status;
+    }
+  }
+  return status;
+}
+console.log(checkAllVowels('aUiueOAiu'));
+console.log(checkAllVowels('akUiueOAiu'));
