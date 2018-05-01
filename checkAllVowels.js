d9547b43e2a7dbaeb6287ca402e037f389e31d36
Rin Ng <Rin08.ng@gmail.com>
2018-05-01 17:28:03 +0700

    Added code for checkAllVowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,33 @@
+function checkVowels(str){
+  var arr = ["a","i","u","e","o"];
+  str = str.toLowerCase();
+  str = str.split("");
+  var count = 0;
 
+
+    for(var i = 0; i < str.length; i++){
+      for(var k = 0; k < arr.length; k++){
+        if(str[i] === arr[k]){
+          count++;
+          // console.log(arr[k]);
+          // console.log(str[i]);
+        }
+      }
+    }
+
+
+    if(count === str.length){
+      return true;
+    }
+    else{
+      return false;
+    }
+
+
+}
+
+console.log(checkVowels("AioUe")); //true
+console.log(checkVowels("AnbUiOe")); //false
+console.log(checkVowels("AbbbbA")); //false
+console.log(checkVowels("jaodijnwNWNw")); //false
+console.log(checkVowels("IiOooOIEeUuei")); //true
