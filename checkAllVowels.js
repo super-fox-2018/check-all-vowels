c84de6c137d55004715d013ff34a1b49ee66c697
handipriyono <handimacbook@gmail.com>
2018-05-01 12:20:28 +0700

    add checkvowels js modified

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkvokal(kata) {
+  var vokal = ['a', 'i', 'u', 'e', 'o'];
 
+  for (let i = 0; i < kata.length; i++) {
+    var cek = false;
+    for (let j = 0; j < vokal.length; j++) {
+      if (kata[i].toLowerCase() === vokal[j]) {
+        cek = true;
+      }
+    }
+
+    if (!cek) {
+      return false;
+    }
+  }
+
+  return true;
+}
+console.log(checkvokal('AUiOe'));
+console.log(checkvokal('oAiude'));
