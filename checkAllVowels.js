37d30bc413cc87c63746c7fcd06df32470a1a5dd
“Ollas2204” <fitrisakinah17@gmail.com>
2018-05-01 23:44:33 +0700

    anchor check all vowel

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,13 @@
+function checkAllVowel(string) {
+    let vocal=['A','I','U','E','O'];
 
+    for(let str of string){
+        if(vocal.indexOf(str.toUpperCase())===-1){
+            return false
+        }
+    }
+    return true;
+}
+
+console.log(checkAllVowel('aAuE')); //true
+console.log(checkAllVowel('aAruE')); //false
