429ede1b6773569b5b6e73f26242398da9cbeac0
unknown <chuddywarrior@gmail.com>
2018-05-06 02:57:27 +0700

    solution for vowel checker

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,17 @@
+function checkAllVowels(string) {
+    const vowels = 'aeiou';
+    let counter = 0;
+    for(var z= 0; z < string.length; z++){
+        for(var k = 0; k < vowels.length; k++){
+            if(string[z].toLowerCase() === vowels[k]){
+                counter++
+            }
+        }
+    }
 
+    return counter === string.length
+    
+  }
+  
+  console.log(checkAllVowels('uoiauai')); // true
+  console.log(checkAllVowels('hacktiv8')); // false
