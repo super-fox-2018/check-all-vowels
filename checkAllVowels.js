4121edd0533db4f8f388ed65ba97e68b0bf8a7f0
“Ollas2204” <fitrisakinah17@gmail.com>
2018-05-21 16:54:51 +0700

    hehe

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,13 +1,23 @@
-function checkAllVowel(string) {
-    let vocal=['A','I','U','E','O'];
+var vocal = ['a', 'i', 'u', 'e', 'o']
 
-    for(let str of string){
-        if(vocal.indexOf(str.toUpperCase())===-1){
-            return false
-        }
+function checkVocal(str) {
+  var jumlah = 0
+
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < vocal.length; j++) {
+      if (str[i] === vocal[j]) {
+        jumlah += 1
+      }
     }
-    return true;
+  }
+  if (jumlah === str.length) {
+    return true
+  } else {
+    return false
+  }
 }
 
-console.log(checkAllVowel('aAuE')); //true
-console.log(checkAllVowel('aAruE')); //false
+console.log(checkVocal('aaaaa'));
+console.log(checkVocal('aiueo'));
+console.log(checkVocal('abbaa'));
+console.log(checkVocal('auiuiuiuaa'));
